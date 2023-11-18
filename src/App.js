edb00dca52148c4f0f4db0c0ac7048d7aa180123
Saieed Shafi <saieedalsaleel@gmail.com>
2023-11-18 16:21:08 +0530

    HowtoUse Tour and Loading
    added Tour for how to use
    added loading indicator to Predict Button

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from "react";
-import { Button, Row, Col, message } from "antd";
+import { Button, Row, Col, message, Tour } from "antd";
 import {
   UndoOutlined,
   ClearOutlined,
@@ -9,17 +9,28 @@ import CanvasDraw from "react-canvas-draw";
 import pica from "pica";
 import * as tf from "@tensorflow/tfjs";
 import "./App.css";
+import { useCallback } from "react";
 
 function App() {
   const canvasRef = useRef(null);
   const [pred, setPred] = useState();
   const [prob, setProb] = useState();
+  const [loadingPredict, setLoadingPredict] = useState(false);
   const picaInstance = pica();
-  let model;
-  async function loadModel() {
-    model = await tf.loadLayersModel("model.json");
+  const [model, setModel] = useState();
+  const results = useRef(null);
+  const canvas = useRef(null);
+  const clearButton = useRef(null);
+  const undoButton = useRef(null);
+  const predictButton = useRef(null);
+  const [open, setOpen] = useState(false);
+
+  // var model;
+  const loadModel = useCallback(async () => {
+    let m = await tf.loadLayersModel("model.json");
+    setModel(m);
     // console.log(model);
-  }
+  }, []);
 
   const saveDrawing = async () => {
     const canvas = canvasRef.current.canvas.drawing;
@@ -54,7 +65,8 @@ function App() {
   };
 
   async function predict() {
-    await loadModel();
+    setLoadingPredict(true);
+    // await loadModel();
     const imageArray = await saveDrawing(); //imamgeArray is if length 784
     // console.log(imageArray);
     if (imageArray) {
@@ -72,6 +84,7 @@ function App() {
     } else {
       message.error("something went wrong!");
     }
+    setLoadingPredict(false);
   }
 
   // now load the model
@@ -85,7 +98,7 @@ function App() {
     tf.serialization.registerClass(L2);
     loadModel();
     // console.log(digit);
-  }, []);
+  }, [loadModel]);
 
   const props = {
     onChange: null,
@@ -113,6 +126,37 @@ function App() {
     mouseZoomFactor: 0.0,
     zoomExtents: { min: 0.33, max: 3 },
   };
+  const steps = [
+    {
+      title: "Draw a Digit Here",
+      description:
+        "Draw a digit inside the canvas! preferably keep some distance from the border.",
+      target: () => canvas.current,
+    },
+    {
+      title: "Predict Digit",
+      description: "Click to Predict the digit drawn on the Canvas!",
+      target: () => predictButton.current,
+    },
+    {
+      title: "Undo Button",
+      description:
+        "Undo the last Drawing on the Canvas Does not affect the current results",
+      target: () => undoButton.current,
+    },
+    {
+      title: "Clear the Drawing!",
+      description: "Clear the canvas and the Results",
+      target: () => clearButton.current,
+    },
+
+    {
+      title: "Results",
+      description:
+        "The Predicted Digit will be shown here along the the Probability of the prediction.",
+      target: () => results.current,
+    },
+  ];
   return (
     <div className="App">
       <Row gutter={[16, 16]}>
@@ -124,10 +168,10 @@ function App() {
         </Col>
       </Row>
 
-      <Row gutter={[16, 16]}>
+      <Row gutter={[16, 16]} style={{ marginTop: 20, marginBottom: 25 }}>
         <Col xs={24} sm={24} md={8}>
           <Row gutter={[16, 16]}>
-            <Col xs={24} sm={24} md={12}>
+            <Col xs={24} sm={24} md={12} ref={results}>
               <Row justify="center">
                 <Col span={24} style={{ textAlign: "center" }}>
                   <span style={{ color: "gray", fontSize: 20 }}>
@@ -153,8 +197,15 @@ function App() {
           </Row>
         </Col>
         <Col xs={24} sm={24} md={16}>
+          <Row justify="center">
+            <Col span={24} style={{ textAlign: "center" }}>
+              <Button type="link" onClick={() => setOpen(true)}>
+                How to Use?
+              </Button>
+            </Col>
+          </Row>
           <Row justify="centers">
-            <Col span={24}>
+            <Col span={24} ref={canvas}>
               <CanvasDraw
                 {...props}
                 ref={canvasRef}
@@ -173,6 +224,7 @@ function App() {
                 }}
                 type="primary"
                 danger
+                ref={clearButton}
               >
                 Clear
               </Button>
@@ -181,6 +233,7 @@ function App() {
               <Button
                 icon={<UndoOutlined />}
                 onClick={() => canvasRef.current.undo()}
+                ref={undoButton}
               >
                 Undo
               </Button>
@@ -190,6 +243,8 @@ function App() {
                 type="primary"
                 icon={<QuestionOutlined />}
                 onClick={predict}
+                loading={loadingPredict}
+                ref={predictButton}
               >
                 Predict Digit
               </Button>
@@ -197,6 +252,7 @@ function App() {
           </Row>
         </Col>
       </Row>
+      <Tour open={open} onClose={() => setOpen(false)} steps={steps} />
     </div>
   );
 }
